9ea7ff2f5cf5135158bca8a3dd207f02234a0ffb
Eduardo Rosas <lalorosas@outlook.com>
2020-02-18 11:50:23 -0600

    Firebase config for iOS & Droid

--- MySubscriptions.iOS/AppDelegate.cs
+++ MySubscriptions.iOS/AppDelegate.cs
@@ -23,6 +23,9 @@ namespace MySubscriptions.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
+
+            Firebase.Core.App.Configure();
+
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
