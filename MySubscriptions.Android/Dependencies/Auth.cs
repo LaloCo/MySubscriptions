bc5a36f65c0e9d0a6a420f5782abcf6da2bb183d
Eduardo Rosas <lalorosas@outlook.com>
2020-02-18 13:25:50 -0600

    Implement Android Auth

---
+++ MySubscriptions.Android/Dependencies/Auth.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Threading.Tasks;
+using Firebase.Auth;
+using MySubscriptions.ViewModel.Helpers;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(Auth))]
+namespace MySubscriptions.Droid.Dependencies
+{
+    public class Auth : IAuth
+    {
+        public Auth()
+        {
+        }
+
+        public async Task<bool> AuthenticateUser(string email, string password)
+        {
+            try
+            {
+                await Firebase.Auth.FirebaseAuth.Instance.SignInWithEmailAndPasswordAsync(email, password);
+
+                return true;
+            }
+            catch (FirebaseAuthWeakPasswordException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            catch (FirebaseAuthInvalidCredentialsException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            catch (FirebaseAuthInvalidUserException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("An unknown error occurred, please try again.");
+            }
+        }
+
+        public string GetCurrentUserId()
+        {
+            return Firebase.Auth.FirebaseAuth.Instance.CurrentUser.Uid;
+        }
+
+        public bool IsAuthenticated()
+        {
+            return Firebase.Auth.FirebaseAuth.Instance.CurrentUser != null;
+        }
+
+        public async Task<bool> RegisterUser(string name, string email, string password)
+        {
+            try
+            {
+                await Firebase.Auth.FirebaseAuth.Instance.CreateUserWithEmailAndPasswordAsync(email, password);
+
+                var profileUpdates = new Firebase.Auth.UserProfileChangeRequest.Builder();
+                profileUpdates.SetDisplayName(name);
+                var build = profileUpdates.Build();
+                var user = Firebase.Auth.FirebaseAuth.Instance.CurrentUser;
+                await user.UpdateProfileAsync(build);
+
+                return true;
+            }
+            catch (FirebaseAuthWeakPasswordException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            catch (FirebaseAuthInvalidCredentialsException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            catch (FirebaseAuthUserCollisionException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("An unknown error occurred, please try again.");
+            }
+        }
+    }
+}
