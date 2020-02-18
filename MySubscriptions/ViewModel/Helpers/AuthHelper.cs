7271a4808f6bdf69e807d3910fa81ee27b7a983c
Eduardo Rosas <lalorosas@outlook.com>
2020-02-18 15:34:20 -0600

    Use Dependency Services and improve UI

--- MySubscriptions/ViewModel/Helpers/AuthHelper.cs
+++ MySubscriptions/ViewModel/Helpers/AuthHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace MySubscriptions.ViewModel.Helpers
 {
@@ -13,16 +14,32 @@ namespace MySubscriptions.ViewModel.Helpers
 
     public class Auth
     {
-        private static IAuth auth;
+        private static IAuth auth = DependencyService.Get<IAuth>();
 
         public static async Task<bool> RegisterUser(string name, string email, string password)
         {
-            return await auth.RegisterUser(name, email, password);
+            try
+            {
+                return await auth.RegisterUser(name, email, password);
+            }
+            catch(Exception ex)
+            {
+                await App.Current.MainPage.DisplayAlert("Error", ex.Message, "Ok");
+                return false;
+            }
         }
 
         public static async Task<bool> AuthenticateUser(string email, string password)
         {
-            return await auth.AuthenticateUser(email, password);
+            try
+            {
+                return await auth.AuthenticateUser(email, password);
+            }
+            catch (Exception ex)
+            {
+                await App.Current.MainPage.DisplayAlert("Error", ex.Message, "Ok");
+                return false;
+            }
         }
 
         public static bool IsAuthenticated()
---
+++ MySubscriptions/ViewModel/LoginVM.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using MySubscriptions.ViewModel.Helpers;
+using Xamarin.Forms;
+
+namespace MySubscriptions.ViewModel
+{
+    public class LoginVM : INotifyPropertyChanged
+    {
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+                OnPropertyChanged("CanRegister");
+            }
+        }
+
+        private string email;
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+                OnPropertyChanged("Email");
+                OnPropertyChanged("CanLogin");
+                OnPropertyChanged("CanRegister");
+            }
+        }
+
+        private string password;
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                password = value;
+                OnPropertyChanged("Password");
+                OnPropertyChanged("CanLogin");
+                OnPropertyChanged("CanRegister");
+            }
+        }
+
+        private string confirmPassword;
+        public string ConfirmPassword
+        {
+            get
+            {
+                return confirmPassword;
+            }
+            set
+            {
+                confirmPassword = value;
+                OnPropertyChanged("ConfirmPassword");
+                OnPropertyChanged("CanRegister");
+            }
+        }
+
+        public bool CanLogin
+        {
+            get
+            {
+                return !string.IsNullOrEmpty(Email) && !string.IsNullOrEmpty(Password);
+            }
+        }
+
+        public bool CanRegister
+        {
+            get
+            {
+                return !string.IsNullOrEmpty(Email) && !string.IsNullOrEmpty(Password) && !string.IsNullOrEmpty(ConfirmPassword) && !string.IsNullOrEmpty(Name);
+            }
+        }
+
+        public ICommand LoginCommand { get; set; }
+        public ICommand RegisterCommand { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public LoginVM()
+        {
+            LoginCommand = new Command(Login, LoginCanExecute);
+            RegisterCommand = new Command(Register, RegisterCanExecute);
+        }
+
+        private bool RegisterCanExecute(object parameter)
+        {
+            return CanRegister;
+        }
+
+        private async void Register(object parameter)
+        {
+            if (ConfirmPassword != Password)
+                await App.Current.MainPage.DisplayAlert("Error", "Passwords do not match", "Ok");
+            else
+                await Auth.RegisterUser(Name, Email, Password);
+        }
+
+        private async void Login(object parameter)
+        {
+            await Auth.AuthenticateUser(Email, Password);
+        }
+
+        private bool LoginCanExecute(object parameter)
+        {
+            return CanLogin;
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
