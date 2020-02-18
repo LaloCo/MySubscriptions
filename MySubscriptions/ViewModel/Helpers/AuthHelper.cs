60b8cf8af03e4e37377cc6cda5d62450f6a84ea2
Eduardo Rosas <lalorosas@outlook.com>
2020-02-18 12:45:49 -0600

    Create interface

---
+++ MySubscriptions/ViewModel/Helpers/AuthHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MySubscriptions.ViewModel.Helpers
+{
+    public interface IAuth
+    {
+        Task<bool> RegisterUser(string name, string email, string password);
+        Task<bool> AuthenticateUser(string email, string password);
+        bool IsAuthenticated();
+        string GetCurrentUserId();
+    }
+
+    public class Auth
+    {
+        private static IAuth auth;
+
+        public static async Task<bool> RegisterUser(string name, string email, string password)
+        {
+            return await auth.RegisterUser(name, email, password);
+        }
+
+        public static async Task<bool> AuthenticateUser(string email, string password)
+        {
+            return await auth.AuthenticateUser(email, password);
+        }
+
+        public static bool IsAuthenticated()
+        {
+            return auth.IsAuthenticated();
+        }
+
+        public static string GetCurrentUserId()
+        {
+            return auth.GetCurrentUserId();
+        }
+    }
+}
