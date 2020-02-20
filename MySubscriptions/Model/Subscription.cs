1f56f2e27a1ea691d1f93a5b9248412ad71f4a11
Eduardo Rosas <lalorosas@outlook.com>
2020-02-20 12:58:56 -0600

    Setup Interface and packages

---
+++ MySubscriptions/Model/Subscription.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace MySubscriptions.Model
+{
+    public class Subscription
+    {
+        public string UserId { get; set; }
+        public string Name { get; set; }
+        public DateTime SubscribedDate { get; set; }
+        public bool IsActive { get; set; }
+
+        public Subscription()
+        {
+        }
+    }
+}
---
+++ MySubscriptions/ViewModel/Helpers/DatabaseHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MySubscriptions.Model;
+
+namespace MySubscriptions.ViewModel.Helpers
+{
+    public interface IFirestore
+    {
+        Task<bool> InsertSubscription(Subscription subscription);
+        Task<bool> DeleteSubscription(Subscription subscription);
+        Task<bool> UpdateSubscription(Subscription subscription);
+        Task<IList<Subscription>> ReadSubscriptions();
+    }
+
+    public class DatabaseHelper
+    {
+        private static IFirestore firestore;
+
+        public Task<bool> DeleteSubscription(Subscription subscription)
+        {
+            return firestore.DeleteSubscription(subscription);
+        }
+
+        public Task<bool> InsertSubscription(Subscription subscription)
+        {
+            return firestore.InsertSubscription(subscription);
+        }
+
+        public Task<IList<Subscription>> ReadSubscriptions()
+        {
+            return firestore.ReadSubscriptions();
+        }
+
+        public Task<bool> UpdateSubscription(Subscription subscription)
+        {
+            return firestore.UpdateSubscription(subscription);
+        }
+    }
+}
